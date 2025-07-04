bdf03f77d1df6608a1c2c2faad9dd2eecf148c41
luckyboy2790 <rorifloris78@gmail.com>
2025-07-04 15:43:37 +0100

    update: update connect db function

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,9 @@ const pool = new Pool({
   database: process.env.DB_NAME || "video_play_management",
   password: process.env.DB_PASSWORD || "password",
   port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on("connect", () => {
